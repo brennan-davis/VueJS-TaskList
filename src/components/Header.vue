1d4942b5aa0d3db0a9f597566dd4cfcd5cb5ba76
brennan-davis <brennan.davis@outlook.com>
2022-10-28 00:54:19 -0400

    Finished localized version of sample VueJS Task List Project

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<Button
+			@toggle-add-task="$emit('toggle-add-task')"
+			:text="showAddTasks ? 'Close' : 'Add Task'"
+			:color="showAddTasks ? 'red' : 'green'"
+		/>
+	</header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "Header",
+	props: {
+		title: String,
+		showAddTasks: Boolean,
+	},
+	components: {
+		Button,
+	},
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
